873fcf03859a6b50ff96e83a756126b93084380c
maheen562 <maheenkhan193@gmail.com>
2024-06-19 18:05:56 +1000

    added removeElement sol

--- mergeSortedArray.py
+++ mergeSortedArray.py
@@ -1,4 +1,8 @@
+""" leetCode problem,
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+"""
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
