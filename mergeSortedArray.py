af6fce73aad68a909c7a49520db7f8f9fabade7d
maheen562 <maheenkhan193@gmail.com>
2024-06-19 17:54:28 +1000

    added mergesortedarray sol

--- mergeSortedArray.py
+++ mergeSortedArray.py
@@ -1,10 +1,9 @@
 
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-       for n in nums1:
-        if 0 in nums1:
-            nums1.remove(0)
+        nums1.extend(nums2)
+        nums1.sort()
 
-       nums1.extend(nums2)
-       nums1.sort() 
-        
+        while len(nums1) > m + n:
+            nums1.remove(0)
