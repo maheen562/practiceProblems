405b44357d74ed08f7588fb66eee81878ade5665
maheen562 <maheenkhan193@gmail.com>
2024-11-03 10:36:53 +1100

    added IncreasingTripleSeq

---
+++ IncreasingTripleSeq.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n1 = Integer.MAX_VALUE;
+        int n2 = Integer.MAX_VALUE;
+
+        for(int n:nums){
+            if (n<=n1) n1 = n;
+            else if (n<=n2) n2=n;
+            else return true;
+        }
+
+        return false;
+    }
+}
